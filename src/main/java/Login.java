e83f82d070228664bc775acf7e7b0073435cb8c1
Zilya <zilya.nurislamova@mail.ru>
2022-02-01 13:00:05 +0300

    updated pom.xml and new chromedriver.exe

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,10 +1,8 @@
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.*;
-import java.util.*;
+import java.time.Duration;
 
 public class Login {
     static WebDriver driver = new ChromeDriver();
