a35a046833db8efd7846969daaa3035a829c321a
Zilya <zilya.nurislamova@mail.ru>
2022-02-02 13:19:11 +0300

    added Performing

---
+++ src/test/java/PerformingTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PerformingTest {
+    @Test
+    public void shouldPerformPayment(){
+        SigningTest paymentToBeSigned = new SigningTest();
+        paymentToBeSigned.shouldSignPayment();
+        Performing.clickSendButton();
+        WebElement successfulSendingNotification = Login.driver.findElement(By.xpath("//h2[text()='Платёжное поручение отправлено']"));
+        assertEquals("Платёжное поручение отправлено", successfulSendingNotification.getText());
+    }
+}
