cce747a657304eda1130b84b20e38e5e77d811d1
Zilya <zilya.nurislamova@mail.ru>
2022-02-02 20:26:28 +0300

    added static to non-static methods

--- src/main/java/PaymentCreation.java
+++ src/main/java/PaymentCreation.java
@@ -4,20 +4,20 @@ import org.openqa.selenium.WebElement;
 
 public class PaymentCreation {
     /** Открыть раздел "Счета и платежи" **/
-    public void openAccountsPaymentsPage(){
+    public static void openAccountsPaymentsPage(){
         WebElement accountsPaymentsOption = Login.driver.findElement(By.cssSelector("button[aria-label=\"Счета и платежи\"]"));
         accountsPaymentsOption.click();
     }
 
     /** Нажать на кнопку "Новый платёж" **/
-    public void clickNewPaymentButton(){
+    public static void clickNewPaymentButton(){
         Login.driver.findElement(By.className("button-wrapper")).click();
         WebElement newPaymentButton = Login.driver.findElement(By.cssSelector(".dropdown-new-payment.dropdown-toggle.btn.btn-success"));
         newPaymentButton.click();
     }
 
     /** Выбрать тип платежа **/
-    public void selectPaymentType(PaymentTypes paymentType){
+    public static void selectPaymentType(PaymentTypes paymentType){
         switch (paymentType) {
             case COMMERCIALPAYMENT:
                 WebElement commercialPaymentOption = Login.driver.findElement(By.cssSelector("ul[role='menu'] li:first-child a"));
@@ -35,9 +35,9 @@ public class PaymentCreation {
         }
 
     /** Открыть форму создания платежа **/
-    public static void openPaymentCreationForm(PaymentCreation accountsPaymentsPage, PaymentTypes paymentType){
-        accountsPaymentsPage.openAccountsPaymentsPage();
-        accountsPaymentsPage.clickNewPaymentButton();
-        accountsPaymentsPage.selectPaymentType(paymentType);
+    public static void openPaymentCreationForm(PaymentTypes paymentType){
+        openAccountsPaymentsPage();
+        clickNewPaymentButton();
+        selectPaymentType(paymentType);
     }
 }
--- src/test/java/PaymentCreationTest.java
+++ src/test/java/PaymentCreationTest.java
@@ -7,9 +7,9 @@ import static org.junit.Assert.assertEquals;
 public class PaymentCreationTest {
     @Test
     public void shouldCreateCommercialPayment(){
-        Login.login(new Login(),"https://sbi.sberbank.ru:9443/ic/dcb/?#/");
-        PaymentCreation.openPaymentCreationForm(new PaymentCreation(), PaymentTypes.COMMERCIALPAYMENT);
-        CommercialPayment.createCommercialPayment(new CommercialPayment(), "77,78", "счет", "контр");
+        Login.login("https://sbi.sberbank.ru:9443/ic/dcb/?#/");
+        PaymentCreation.openPaymentCreationForm(PaymentTypes.COMMERCIALPAYMENT);
+        CommercialPayment.createCommercialPayment("77,78", "счет", "контр");
         WebElement successfulCreationNotification = Login.driver.findElement(By.xpath("//h2[text()='Платёжное поручение создано']"));
         assertEquals("Платёжное поручение создано", successfulCreationNotification.getText());
         System.out.println("Платёж контрагенту успешно создан");
@@ -17,9 +17,9 @@ public class PaymentCreationTest {
 
     @Test
     public void shouldCreateBudgetPayment(){
-        Login.login(new Login(),"https://sbi.sberbank.ru:9443/ic/dcb/?#/");
-        PaymentCreation.openPaymentCreationForm(new PaymentCreation(), PaymentTypes.BUDGETPAYMENT);
-        BudgetPayment.createBudgetPayment(new BudgetPayment(), "77,78", "18210101011011000110", "19730000", "0", "777", "0");
+        Login.login("https://sbi.sberbank.ru:9443/ic/dcb/?#/");
+        PaymentCreation.openPaymentCreationForm(PaymentTypes.BUDGETPAYMENT);
+        BudgetPayment.createBudgetPayment("77,78", "18210101011011000110", "19730000", "0", "777", "0");
         WebElement successfulCreationNotification = Login.driver.findElement(By.xpath("//h2[text()='Платёжное поручение создано']"));
         assertEquals("Платёжное поручение создано", successfulCreationNotification.getText());
         System.out.println("Платёж в бюджет успешно создан");
@@ -27,9 +27,9 @@ public class PaymentCreationTest {
 
     @Test
     public void shouldCreatePaymentBetweenAccounts(){
-        Login.login(new Login(),"https://sbi.sberbank.ru:9443/ic/dcb/?#/");
-        PaymentCreation.openPaymentCreationForm(new PaymentCreation(), PaymentTypes.BETWEENACCOUNTS);
-        PaymentBetweenAccounts.createPaymentBetweenAccounts(new PaymentBetweenAccounts(), "22988,50");
+        Login.login("https://sbi.sberbank.ru:9443/ic/dcb/?#/");
+        PaymentCreation.openPaymentCreationForm(PaymentTypes.BETWEENACCOUNTS);
+        PaymentBetweenAccounts.createPaymentBetweenAccounts("22988,50");
         WebElement successfulCreationNotification = Login.driver.findElement(By.xpath("//h2[text()='Платёжное поручение создано']"));
         assertEquals("Платёжное поручение создано", successfulCreationNotification.getText());
         System.out.println("Платёж между своими счетами успешно создан");
