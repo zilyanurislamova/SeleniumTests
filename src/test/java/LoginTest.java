b245953a9fbabef2483d6dc38bd00b68cfa4e54f
Zilya <zilya.nurislamova@mail.ru>
2022-02-05 14:06:35 +0300

    updated dependencies (junit4->junit5)

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,17 +1,20 @@
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginTest {
     @Test
-    public void shouldLogin() {
+    @DisplayName("Вход в демо-режим")
+    void shouldLogin() {
         Login.login("https://sbi.sberbank.ru:9443/ic/dcb/?#/");
         new WebDriverWait(Login.driver, Duration.ofSeconds(10)).
                 until(ExpectedConditions.urlToBe("https://sbi.sberbank.ru:9444/ic/dcb/index.html#/main"));
         assertEquals(Login.driver.getCurrentUrl(), "https://sbi.sberbank.ru:9444/ic/dcb/index.html#/main");
+        System.out.println("Выполнен вход в демо-режим");
     }
 }
